63ac89b57cb96cb7c5b5fafe7e9af719104e93e7
lukaskloc <lukas.kloc@gmail.com>
2020-03-10 23:11:35 +0100

    2020.03.10

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("First test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(3, 4);
+
+        if(addResult == 7) {
+            System.out.println("Second test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        int subtractResult = calculator.subtract(4, 3);
+
+        if(subtractResult == 1) {
+            System.out.println("Third test ok");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
