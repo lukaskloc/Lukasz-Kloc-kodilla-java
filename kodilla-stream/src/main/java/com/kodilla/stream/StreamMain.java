a03215edad4974dfe42f3290502fc669f637a467
lukaskloc <lukas.kloc@gmail.com>
2020-03-19 10:48:12 +0100

    2020-03-19

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,36 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references.");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        final String poemToDecorate = "Sample poem to decorate";
+
+        poemBeautifier.beautify(poemToDecorate, toDecorate -> "XYZ" + poemToDecorate + "XYZ" );
+        poemBeautifier.beautify(poemToDecorate, toDecorate -> poemToDecorate.toUpperCase());
+        poemBeautifier.beautify(poemToDecorate, toDecorate -> poemToDecorate.replace('a', 'A'));
+        poemBeautifier.beautify(poemToDecorate, toDecorate -> poemToDecorate.substring(7));
+    }
+}
