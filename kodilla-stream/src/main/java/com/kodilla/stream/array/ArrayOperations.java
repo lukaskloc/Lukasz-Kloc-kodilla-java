22afa2078497272ebebc2c580b1beee006c42d51
lukaskloc <lukas.kloc@gmail.com>
2020-03-21 15:57:12 +0100

    2020-03-21.2

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,16 @@
+package com.kodilla.stream.array;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+    static double getAverage(int[] numbers) {
+        IntStream.range(0, numbers.length)
+                .map(x -> numbers[x])
+                .forEach(System.out::println);
+        return IntStream.range(0, numbers.length)
+                .map(x -> numbers[x])
+                .average()
+                .orElse(0);
+    }
+}
