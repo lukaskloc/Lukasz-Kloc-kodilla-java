757fecc0f099adb9c1081573034e0f5b8290bfb7
lukaskloc <lukas.kloc@gmail.com>
2020-05-15 10:39:16 +0200

    2020.05.15

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -0,0 +1,45 @@
+package com.kodilla.patterns.strategy.social;
+
+import com.kodilla.patterns.strategy.social.publishers.FacebookPublisher;
+import com.kodilla.patterns.strategy.social.publishers.SnapchatPublisher;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTestSuite {
+    @Test
+    public void testDefaultSharingStrategies() {
+        //Given
+        User dzesika = new Millenials("Dżesika Johnson");
+        User brajan = new YGeneration("Brajan Brandon");
+        User andzela = new ZGeneration("Andżelika Jelly");
+
+        //When
+        String dzesikaStrategyToShare = dzesika.sharing();
+        System.out.println("Dżesika: " + dzesikaStrategyToShare);
+        String brajanStrategyToShare = brajan.sharing();
+        System.out.println("Brajan: " + brajanStrategyToShare);
+        String andzelaStrategyToShare = andzela.sharing();
+        System.out.println("Andżela: " + andzelaStrategyToShare);
+
+        //Then
+        Assert.assertEquals("[Social publisher] Using Facebook to publish", dzesikaStrategyToShare);
+        Assert.assertEquals("[Social publisher] Using Twitter to publish", brajanStrategyToShare);
+        Assert.assertEquals("[Social publisher] Using Snapchat to publish", andzelaStrategyToShare);
+    }
+
+    @Test
+    public void testIndividualSharingStrategy() {
+        //Given
+        User dzesika = new Millenials("Dżesika Johnson");
+
+        //When
+        String dzesikaStrategyToShare = dzesika.sharing();
+        System.out.println("Dżesika: " + dzesikaStrategyToShare);
+        dzesika.setPublishingStrategy(new SnapchatPublisher());
+        dzesikaStrategyToShare = dzesika.sharing();
+        System.out.println("Dżesika from now on: " + dzesikaStrategyToShare);
+
+        //Then
+        Assert.assertEquals("[Social publisher] Using Snapchat to publish", dzesikaStrategyToShare);
+    }
+}
